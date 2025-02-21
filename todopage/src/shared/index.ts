da6370c7615b1da738efb6b46bb2d10b6c2a74bd
권형석 <hskwon517@gmail.com>
2025-02-21 11:37:08 +0900

    chore: badge 컴포넌트를 shared 레이어로 이동

--- todopage/src/shared/index.ts
+++
@@ -1,3 +0,0 @@
-export { Card } from "./ui/card"
-export { Button } from "./ui/button"
-export { Dialog, DialogTrigger, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter, DialogClose } from "./ui/dialog"
