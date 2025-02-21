8288b79ce7ef813517d3c644a73b405aec7688c9
권형석 <hskwon517@gmail.com>
2025-02-21 10:19:32 +0900

    feat: FSD 페이지 레이어에 TodoPage 추가 및 index.ts로 export 후 app/page.tsx에서 import

---
+++ todopage/src/pages/ui/todo-page.tsx
@@ -0,0 +1,7 @@
+export const TodoPage = () => {
+  return (
+    <div className="max-w-7xl mx-auto w-full bg-red-300 h-full">
+      <h1 className="font-bold text-5xl">Todo List</h1>
+    </div>
+  )
+}
