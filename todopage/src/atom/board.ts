b196c7f64b86aea50055c63448e1b22e2f5b7ba0
권형석 <hskwon517@gmail.com>
2025-02-21 11:38:01 +0900

    chore: 샘플 tasks 데이터를 atom에 추가

--- todopage/src/atom/board.ts
+++ todopage/src/atom/board.ts
@@ -10,11 +10,34 @@ export type Board = {
 export type Column = {
   id: string;
   title: string;
+  color: string;
   tasks: Board[];
 }
 
 export const boardAtom = atomWithStorage<Column[]>("board", [
-  {id: "todo", title: "To-do", tasks: []},
-  {id: "in-progress", title: "In Progress", tasks: []},
-  {id: "done", title: "Done", tasks: []},
+  {
+    id: "todo",
+    title: "To-do",
+    color: "#ffd3d6",
+    tasks: [
+      {
+        id: "1",
+        title: "Task 1",
+        content: "Content 1",
+        createdAt: "2021-01-01",
+      },
+    ],
+  },
+  {
+    id: "in-progress",
+    title: "In Progress",
+    color: "#fbefd2",
+    tasks: [],
+  },
+  {
+    id: "done",
+    title: "Done",
+    color: "#d1e9e3",
+    tasks: [],
+  },
 ])
