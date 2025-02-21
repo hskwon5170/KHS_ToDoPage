8c054135fff77f117e872faa17f1b9dceb9ab800
권형석 <hskwon517@gmail.com>
2025-02-21 16:01:23 +0900

    chore: board-list 컴포넌트 코드 스타일 수정

--- todopage/src/widgets/ui/board-list.tsx
+++ todopage/src/widgets/ui/board-list.tsx
@@ -1,6 +1,6 @@
 import type { Column } from "@/atom/board"
 
-import { Board } from "@/features/ui/board"
+import { Board, CreateBoardButton } from "@/features/index";
 import { Badge } from "@/shared/ui/badge"
 
 type BoardListProps = {
@@ -10,20 +10,29 @@ type BoardListProps = {
 export const BoardList = ({
   column,
 }: BoardListProps) => {
-
   return (
-    <div className="flex flex-col gap-2 items-start">
-      <div className="flex">
-        <Badge
-          className="rounded-sm text-slate-950 w-fit"
-          style={{ backgroundColor: column.color }}
-        >
-          {column.title}
-        </Badge>
-      </div>
-      {column.tasks.map((task) => (
-        <Board key={task.id} task={task} />
-      ))}
-    </div>
+    <section className="flex flex-col gap-2 items-start">
+      <header className="flex items-center w-full justify-between gap-4">
+        <div className="flex items-center gap-4 flex-1">
+          <Badge className="rounded-sm text-slate-950 w-fit" style={{ backgroundColor: column.color }}>
+            {column.category}
+          </Badge>
+          <div className="text-slate-400">
+            {column.tasks.length}
+          </div>
+        </div>
+        {column.id === "todo-column" && (
+          <CreateBoardButton />
+        )}
+      </header>
+
+      <ul className="w-full flex flex-col gap-2">
+        {column.tasks.map((task) => (
+          <li key={task.id}>
+            <Board task={task} category={column.category} color={column.color || ""} />
+          </li>
+        ))}
+      </ul>
+    </section>
   )
 }
