f7cc1a2bc2406e0e4a378af2320397102a49fe6b
권형석 <hskwon517@gmail.com>
2025-02-23 22:19:39 +0900

    feat: fallback card 렌더를 위한 예외처리

--- todopage/src/widgets/ui/board-list.tsx
+++ todopage/src/widgets/ui/board-list.tsx
@@ -1,7 +1,17 @@
-import type { Column } from "@/atom/board"
+"use client"
 
-import { Board, CreateBoardButton } from "@/features/index";
+import { boardAtom, type Column } from "@/atom/board"
+
+import { useRef } from "react"
+import { useDrop } from "react-dnd"
+import { useAtom } from "jotai/react"
+
+import { cn } from "@/lib/utils"
+
+import { Board, CreateBoardButton } from "@/features/index"
 import { Badge } from "@/shared/ui/badge"
+import { FallbackBoard } from "@/entities"
+import { toast } from "sonner"
 
 type BoardListProps = {
   column: Column;
@@ -10,11 +20,53 @@ type BoardListProps = {
 export const BoardList = ({
   column,
 }: BoardListProps) => {
+  const ref = useRef<HTMLElement>(null)
+  const [board, setBoard] = useAtom(boardAtom)
+
+  const [{ isOver }, drop] = useDrop(() => ({
+    accept: "BOARD",
+    drop: (item: { id: string, sourceColumn: string }) => {
+      const sourceColumnIndex = board.findIndex(col => col.category === item.sourceColumn)
+      const targetColumnIndex = board.findIndex(col => col.category === column.category)
+
+      if (sourceColumnIndex !== -1 && targetColumnIndex !== -1) {
+        const taskToMove = board[sourceColumnIndex].tasks.find(t => t.id === item.id)
+
+        if (taskToMove) {
+          const newBoard = [...board]
+
+          newBoard[sourceColumnIndex] = {
+            ...newBoard[sourceColumnIndex],
+            tasks: newBoard[sourceColumnIndex].tasks.filter(t => t.id !== item.id)
+          }
+
+          newBoard[targetColumnIndex] = {
+            ...newBoard[targetColumnIndex],
+            tasks: [...newBoard[targetColumnIndex].tasks, taskToMove]
+          }
+
+          setBoard(newBoard)
+        }
+        toast("선택한 보드의 상태가 변경되었습니다.")
+      }
+    },
+    collect: (monitor) => ({
+      isOver: monitor.isOver(),
+    })
+  }), [board, column.category])
+  drop(ref)
+
   return (
-    <section className="flex flex-col gap-2 items-start">
-      <header className="flex items-center w-full justify-between gap-4">
-        <div className="flex items-center gap-4 flex-1">
-          <Badge className="rounded-sm text-slate-950 w-fit" style={{ backgroundColor: column.color }}>
+    <section
+      ref={ref}
+      className={cn(
+        "flex flex-col gap-2 items-start",
+        isOver && "bg-slate-100"
+      )}
+    >
+      <header className="flex h-10 w-full items-center justify-between gap-4">
+        <div className="flex flex-1 items-center gap-4">
+          <Badge className="w-fit rounded-sm text-slate-950" style={{ backgroundColor: column.color }}>
             {column.category}
           </Badge>
           <div className="text-slate-400">
@@ -26,12 +78,16 @@ export const BoardList = ({
         )}
       </header>
 
-      <ul className="w-full flex flex-col gap-2">
-        {column.tasks.map((task) => (
-          <li key={task.id}>
-            <Board task={task} category={column.category} color={column.color || ""} />
-          </li>
-        ))}
+      <ul className="flex w-full flex-col gap-2">
+        {column.tasks.length > 0 ? (
+          column.tasks.map((task) => (
+            <li key={task.id}>
+              <Board task={task} category={column.category} color={column.color || ""} />
+            </li>
+          ))
+        ) : column.id === "todo-column" ? (
+          <FallbackBoard />
+        ) : null}
       </ul>
     </section>
   )
