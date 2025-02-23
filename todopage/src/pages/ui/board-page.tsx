04fcfdb3d257b5b50ea6d0530081d6a250266fb0
권형석 <hskwon517@gmail.com>
2025-02-23 22:19:12 +0900

    fix: 레이아웃 타이틀 스타일 수정

--- todopage/src/pages/ui/board-page.tsx
+++ todopage/src/pages/ui/board-page.tsx
@@ -5,8 +5,9 @@ import { DndProvider } from "react-dnd"
 import { HTML5Backend } from "react-dnd-html5-backend"
 
 import { boardAtom } from "@/atom/board"
-
 import { BoardList } from "@/widgets/index"
+import { FallbackBoard } from "@/entities"
+import { Kanban } from "lucide-react"
 
 
 export const BoardPage = () => {
@@ -14,8 +15,11 @@ export const BoardPage = () => {
 
   return (
     <DndProvider backend={HTML5Backend}>
-      <div className="mx-auto flex size-full max-w-7xl flex-col gap-4">
-        <h1 className="text-5xl font-bold">To-do</h1>
+      <div className="mx-auto flex size-full max-w-7xl flex-col gap-12">
+        <h1 className="flex items-center gap-3 text-4xl font-bold text-gray-800">
+          <Kanban className="size-10 text-indigo-600" />
+          Kanban Todo
+        </h1>
         <div className="grid grid-cols-3 gap-8">
           {columns.map((column) => (
             <BoardList key={column.id} column={column} />
